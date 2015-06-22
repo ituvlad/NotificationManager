b50ef881ea4111c93148a017278facc87a3539d3
Vlad Itu <itu.vlad@gmail.com>
2015-06-22 22:24:52 +0300

    Tried a different way - led is still not updated

--- app/src/main/java/com/manager/notification/notificationmanager/NotificationListener.java
+++ app/src/main/java/com/manager/notification/notificationmanager/NotificationListener.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Build;
 import android.service.notification.NotificationListenerService;
@@ -17,17 +18,33 @@ import android.util.Log;
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class NotificationListener extends NotificationListenerService {
 
+    private int handledId;
+
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         super.onNotificationPosted(sbn);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setLights(Color.WHITE, 1000, 300);
-        Notification notification = builder.build();
-        mNotificationManager.notify(sbn.getId(), notification);
+        if(sbn.getId() != handledId ) {
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            Notification.Builder builder = new Notification.Builder(this);
+            builder.setPriority(Notification.PRIORITY_MAX);
+            builder.setDefaults(Notification.DEFAULT_LIGHTS);
+            //builder.setLights(Color.WHITE, 1, 1);
+            builder.setSmallIcon(R.drawable.ic_money_bag);
+            Notification notification = builder.build();
+            notification.ledARGB = Color.WHITE;
+            notification.ledOnMS = 500;
+            notification.ledOffMS = 500;
+            mNotificationManager.notify(sbn.getId(), notification);
 
-        Log.e("!!!!!!!!!!!!!!!!!!!!!!!", "led on: " + notification.ledOnMS);
+            Log.e("!!!!!!!!!!!!!!!!!!!!!!!", "led on: " + notification.ledOnMS);
+            handledId = sbn.getId();
+        }
     }
 
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        super.onNotificationRemoved(sbn);
+    }
 }
+
